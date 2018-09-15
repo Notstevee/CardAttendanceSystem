6c451edde2564f7233711ce10deb86f70c8c3ce3
Joch2520 <20432565+Joch2520@users.noreply.github.com>
2018-09-15 16:33:27 +0800

    Added repeat until loop which is tagged comment

--- TempDataSave.pas
+++ TempDataSave.pas
@@ -11,16 +11,17 @@ var
 begin
 	Assign(DSout, DSFname); //to declare the text output 'DSout' to the file path
 	
-	//currently the program only do the following procedure once, add a do until loop if it is to be run forever
-	
-	readln(cardid);
-	try
-		Append(DSout); //to write to the file without deleting previous records
-		writeln(DSout, cardid); //to enter the card id
-		writeln(DSout, Time); //to enter the current time in a way readable by computer
-		Close(DSout); //to save the file
-	except
-		on E: EInOutError do //to catch the error
-			writeln('File handling error occurred. Details: ', E.ClassName, '/',E.Message);
-		end;
+	//currently the program only do the following procedure once, remove comment tag for until loop if it is to be run forever
+	//repeat
+		readln(cardid);
+		try
+			Append(DSout); //to write to the file without deleting previous records
+			writeln(DSout, cardid); //to enter the card id
+			writeln(DSout, Time); //to enter the current time in a way readable by computer
+			Close(DSout); //to save the file
+		except
+			on E: EInOutError do //to catch the error
+				writeln('File handling error occurred. Details: ', E.ClassName, '/',E.Message);
+			end;
+	//until (LowerCase(cardid) = 'exit') or (LowerCase(cardid) = 'stop') or (LowerCase(cardid) = 'terminate');
 end.
